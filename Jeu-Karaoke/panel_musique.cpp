f505fd6b308b067d7d90d69294350c348e22614a
Alex <alexandre.m.halley@gmail.com>
2012-04-20 20:20:18 +0200

    Les outils phonon ne sont pas identifiés...

--- Jeu-Karaoke/panel_musique.cpp
+++ Jeu-Karaoke/panel_musique.cpp
@@ -3,8 +3,126 @@
 Panel_Musique::Panel_Musique(QWidget *parent) :
     QWidget(parent)
 {
+
+    /*
     QPalette Pal(palette());
     Pal.setColor(QPalette::Background, Qt::blue);
     setPalette(Pal);
     setAutoFillBackground(true);
+    */
+
+    // Gestion du fichier audio
+    audioOutput = new Phonon::AudioOutput(Phonon::MusicCategory,this);
+    mediaObject = new Phonon::MediaObject(this);
+
+    mediaObject->setTickInterval(1000); // Emission d'un tick toutes les secondes
+    connect(mediaObject, SIGNAL(tick(qint64)),this, SLOT(tick(qint64))); // On va passer le temps écoulé (tick) à l'attribut "temps"
+
+    source = "Sound/1995_LaSuite.wav";
+    mediaObject->setCurrentSource(source);
+
+    Phonon::createPath(mediaObject,audioOutput);    //Relie le gestionnaire du son au diffuseur de son
+
+    setActions();
+    setStateMachine();
+    setUI();
+}
+
+Panel_Musique::~Panel_Musique()
+{
+    //delete ui;
+}
+
+void Panel_Musique::tick(qint64 time){
+    QTime displayTime(00, ( time / 60000 ) % 60, ( time / 1000 ) % 60,0);
+    temps->display(displayTime.toString("mm:ss"));
+}
+
+void Panel_Musique::setActions(){
+
+    playAction = new QAction(tr("Play"),this);
+    playAction->setDisabled(false);
+    pauseAction = new QAction(tr("Pause"),this);
+    pauseAction->setDisabled(false);
+    stopAction = new QAction(tr("Stop"),this);
+    stopAction->setDisabled(false);
+
+}
+
+void Panel_Musique::setStateMachine(){
+
+    mac = new QStateMachine() ;
+    playState = new QState();
+    pauseState = new QState();
+    stopState = new QState();
+
+    playState->addTransition(pauseAction,SIGNAL(triggered()),pauseState);
+    playState->addTransition(stopAction,SIGNAL(triggered()),stopState);
+
+    pauseState->addTransition(playAction,SIGNAL(triggered()),playState);
+    pauseState->addTransition(stopAction,SIGNAL(triggered()),stopState);
+
+    stopState->addTransition(playAction,SIGNAL(triggered()),playState);
+    stopState->addTransition(pauseAction,SIGNAL(triggered()),pauseState);
+
+    mac->addState(playState);
+    mac->addState(pauseState);
+    mac->addState(stopState);
+    mac->setInitialState(pauseState);
+
+    QObject::connect(playState, SIGNAL(entered()), mediaObject, SLOT(play()));
+    QObject::connect(pauseState, SIGNAL(entered()), mediaObject, SLOT(pause()));
+    QObject::connect(stopState, SIGNAL(entered()), mediaObject, SLOT(stop()));
+
+    mac->start();
+}
+
+void Panel_Musique::debug(){
+    qDebug() <<"DEBUG";
+
+}
+
+void Panel_Musique::setUI(){
+
+    QHBoxLayout * principalLayout= new QHBoxLayout;
+
+    QVBoxLayout *  interactiveLayout = new QVBoxLayout;
+
+    barreLecture = new Phonon::SeekSlider(this);
+    barreLecture->setMediaObject(mediaObject);
+
+    QHBoxLayout * controlLayout = new QHBoxLayout;
+
+    QToolBar * barreControle = new QToolBar;    // Barre d'outils Pause, Play, Stop
+    barreControle->addAction(playAction);
+    barreControle->addAction(pauseAction);
+    barreControle->addAction(stopAction);
+
+    barreVolume = new Phonon::VolumeSlider(this);
+    barreVolume->setAudioOutput(audioOutput);
+
+    controlLayout->addWidget(barreControle);
+    controlLayout->addWidget(barreVolume);
+
+    interactiveLayout->addWidget(barreLecture);
+    interactiveLayout->addLayout(controlLayout);
+
+    QVBoxLayout* informationLayout= new QVBoxLayout; // Barre d'information : Titre, temps écoulé
+
+    QLabel* titre = new QLabel(source);
+    temps = new QLCDNumber;
+    temps->display("Press Start");
+
+    informationLayout->addWidget(titre);
+    informationLayout->addWidget(temps);
+
+    principalLayout->addLayout(interactiveLayout);
+    principalLayout->addLayout(informationLayout);
+
+    /*
+    QWidget * widget = new QWidget();
+    widget->setLayout(principalLayout);
+*/
+    this->setLayout(principalLayout);
+    //setWindowTitle("SoundReader");
 }
--- Jeu-Karaoke/panel_musique.h
+++ Jeu-Karaoke/panel_musique.h
@@ -2,6 +2,22 @@
 #define PANEL_MUSIQUE_H
 
 #include <QWidget>
+#include <phonon/mediaobject.h>
+#include <phonon/seekslider.h>
+#include <phonon/audiooutput.h>
+#include <phonon/volumeslider.h>
+#include <Phonon/AudioOutput>
+
+#include <QAction>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QState>
+#include <QStateMachine>
+#include <QtDebug>
+#include <QLCDNumber>
+#include <QTime>
+#include <QToolBar>
 
 class Panel_Musique : public QWidget
 {
@@ -9,10 +25,31 @@ class Panel_Musique : public QWidget
 public:
     explicit Panel_Musique(QWidget *parent = 0);
 
-signals:
+    Phonon::SeekSlider *barreLecture;   // Barre de lecture
+    Phonon::MediaObject *mediaObject;   // Permet de gérer la musique
+    Phonon::AudioOutput *audioOutput;   // Permet d'écouter la musique
+    Phonon::VolumeSlider *barreVolume;  //Barre de volume
+    QAction *playAction;
+    QAction *pauseAction;
+    QAction *stopAction;
+    QString source;
+    QStateMachine * mac;
+    QState * playState,*pauseState,*stopState;
+    QLCDNumber *temps;
 
+public:
+    ~Panel_Musique();
+
+    void setUI();           // Interface Utilisateur
+    void setActions();      // Initilialise les QActions
+    void setStateMachine();  // Initialise la machine à état
 public slots:
-
+    void debug();
+    void tick(qint64 time);
+/*
+protected:
+    void changeEvent(QEvent *e);
+*/
 };
 
 #endif // PANEL_MUSIQUE_H
